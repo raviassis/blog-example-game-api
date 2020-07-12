184a11f79394e6d163fe3db5e96a0a5c774db922
Ravi Assis <ravi.g.assis@gmail.com>
2020-07-12 12:19:45 -0300

    Add comment

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,10 +1,11 @@
 const gamesMock = require('../data/gamesMock.json');
 module.exports = {
+    /* List all games from mock file */
     getAll: (req, res) => {
         res.status(200).json(gamesMock);
     },
+    /* Example of a create game method */
     create: (req, res) => {
-
         res.status(201).json(req.body);
     }
 }
