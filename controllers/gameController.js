ef4b26930dab8e36bf2da9725a33b73737de17b2
Ravi Assis <ravi.g.assis@gmail.com>
2020-07-21 21:09:53 -0300

    Validando requisição com structure

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,4 +1,19 @@
 const gamesMock = require('../data/gamesMock.json');
+const { attributes } = require('structure'); // import structure
+ 
+/* Game validation request */
+const GameValidation = attributes({
+   name: {
+       type: String,
+       required: true,
+   },
+   price: {
+       type: Number,
+       positive: true,
+       required: true,
+   }
+})(class GameValidation {});
+
 module.exports = {
     /* List all games from mock file */
     getAll: (req, res) => {
@@ -6,6 +21,13 @@ module.exports = {
     },
     /* Example of a create game method */
     create: (req, res) => {
-        res.status(201).json(req.body);
+        const newGame = new GameValidation(req.body);
+        const {valid, errors} = newGame.validate();
+        if (valid) {
+            res.status(201).json(newGame);
+        }
+        else {
+            res.status(422).json(errors);
+        }
     }
 }
